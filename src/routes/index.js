afe438ed7344cfac02e576201ad54edac9ffeefe
navkgit <69081339+navkgit@users.noreply.github.com>
2023-08-04 00:48:07 +0530

    Add files via upload

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import { signUpRoute } from './signUpRoute';
+import { logInRoute } from './logInRoute';
+import { testRoute } from './testRoute';
+import { updateUserInfoRoute } from './updateUserInfoRoute';
+
+export const routes = [
+    signUpRoute,
+    logInRoute,
+    updateUserInfoRoute,
+    testRoute,
+];
